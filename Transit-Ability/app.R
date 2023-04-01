8bb076ef797eebedaab0c455ee13b76e959d9f7f
S7LEONNIE <la9354@hotmail.com>
2023-04-01 17:43:01 +0800

    RShiny EDA Map

--- Transit-Ability/app.R
+++ Transit-Ability/app.R
@@ -1,50 +1,51 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+pacman::p_load(shiny, sf, tidyverse, tmap, DT)
 
-library(shiny)
+mrt <- readRDS("data/model/mrt_sf.rds")
+mrt_rail <- readRDS("data/model/rail_sf.rds")
+tourism <- readRDS("data/model/tourism_sf.rds")
+shopping <- readRDS("data/model/shopping_sf.rds")
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("EDA - Visualisation"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            selectInput(
+              inputId = "variable",
+              label = "Mapping Variable:",
+              choices = c("Tourist Attraction" = "tourism",
+                          "Shopping Malls" = "shopping"),
+              selected = "tourism"
+            ),
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+          tmapOutput("mapPlot",
+                     width = "100%",
+                     height = 400),
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  output$mapPlot <- renderTmap({
+    tmap_options(check.and.fix = TRUE) +
+      tm_shape(mrt_rail) +
+      tm_lines(lty = "solid", scale = 1) +
+      tm_shape(mrt) +
+      tm_dots(alph=0.5, size=0.07) +
+      tm_shape(get(input$variable)) +
+      tm_dots(size = 0.1, col = 'yellow') +
+      tm_view(set.zoom.limits = c(11,14))
+  })
 }
 
 # Run the application 
